04c672ffb55a47502ff0be5a73216d846ec6ae47
dilshan.d3 <dilshan72404@gmail.com>
2024-07-02 17:55:10 +0530

    kuch nhi kiya bs asynHandler file ko dobara wese hi likha jese pehle thi

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -8,16 +8,3 @@ const asyncHandler = (requestHandler) => {
 
 export { asyncHandler }
 
-/*
-const asynHandler = (fn) => async (req,res,next) => {
-    try{
-    await fn(req,res,next)
-    }
-    catch (error){
-       res.status(err.code || 500.json({
-        succes : false,
-        message : err.message
-       }))
-    }
-}
-*/
